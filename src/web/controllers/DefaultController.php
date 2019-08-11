6977701504b837cec18ac058bae9ef47a800eee6
Vitaliy <theyamshikov@yandex.ru>
2019-08-12 00:35:33 +0600

    feat: make test page

--- src/web/controllers/DefaultController.php
+++ src/web/controllers/DefaultController.php
@@ -3,21 +3,18 @@
 namespace yii2bundle\dashboard\web\controllers;
 
 use yii\web\Controller;
-use yii2rails\domain\data\Query;
-use yii2lab\notify\domain\entities\SmsEntity;
 
 class DefaultController extends Controller
 {
 	
 	public function actionIndex()
 	{
-		//prr(\App::$domain->file->person->all(),1,1);
-		/*$sms = new SmsEntity;
-		$sms->address = '777712345678';
-		$sms->content = 'wertyu';
-		//\App::$domain->notify->sms->sendEntity($sms);
-		//\App::$domain->notify->sms->directSendEntity($sms);*/
-		return $this->render('index');
+        return $this->render('index', ['data' => '']);
 	}
+
+    public function actionTest()
+    {
+        return $this->render('test', ['data' => '']);
+    }
 
 }
--- src/web/views/default/index.php
+++ src/web/views/default/index.php
@@ -4,12 +4,16 @@
 
 $this->title = Yii::t('dashboard/main', 'title');
 
+$data = empty($data) ? EMP : $data;
+
 ?>
 
 <div class="welcome-index">
 
 	<div class="jumbotron">
 		<h1><?= Yii::t('dashboard/main', 'hello') ?></h1>
+
+        <?= $data ?>
 
 		<p class="lead"><?= Yii::t('dashboard/main', 'text') ?></p>
 	</div>
